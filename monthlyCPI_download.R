270509ffcad8ac583d6a517799805a024001e4f5
Sai Sawat <52126418+s-saisw@users.noreply.github.com>
2019-10-08 13:22:14 +0900

    combine provincial data and export as dta

--- monthlyCPI_download.R
+++ monthlyCPI_download.R
@@ -1,32 +1,45 @@
+# =========================================================================
+# This program downloads API files from MOC website and combines price index
+# Source  - "https://data.moc.go.th/OpenData/CPIPIndexes"
+# Process - download API of 75 provinces
+#         - download Beungkan's file
+#         - download Bangkok Metropolitan's file
+#         - combine provincial data and export as dta
+# =========================================================================
+
 library(httr)
 library(jsonlite)
 library(lubridate)
+library(readxl)
+library(dplyr)
+library(haven)
 
 options(stringsAsFactors = FALSE)
 
 setwd("/Data/monthlyCPI")
 
-# construct URL ============================================================================
-provinceCode <- read_excel("/Data/Basic_data/Province_match.xlsx")
-provinceCodeVec <- unique(provinceCode$code)
-rm(provinceCode)
+# choose price to download =================================================================
+id = 1111 #rice
+id = 1140 #fruits and vegetables
+id = 1160 #non-alcoholic drinks
+id = 1220 #eatOutPrice
 
-provinceCode_exceptBangkok <- provinceCodeVec[-c(1)]
+id = 8200 #food&beverage(excluding fresh food)
+id = 9100 #fresh food
 
-id = 122 #eatOutPrice
-i=13
+id = 2000 #clothes and shoes
+id = 2100 #clothes
+id = 2200 #shoes
 
+id = 3700 #domestic worker available only from 2012 -2017
+id = 4220 #private service
+
+# download API and export by province ====================================================
 base1 <- "https://dataapi.moc.go.th/cpip-indexes?province_code="
 base2 <- "&index_id="
-base3 <- "0000000000000&from_year=2003&to_year=2017"
+base3 <- "000000000000&from_year=2003&to_year=2017"
 
-#https://dataapi.moc.go.th/cpip-indexes?province_code=13&index_id=1220000000000000&from_year=2003&to_year=2017"
-
-#c(11:27,30:58, 60:67, 70:77, 80:86, 90:96)
-
-#error: 10, 38
-
-for (i in c(11:27,30:58, 60:67, 70:77, 80:86, 90:96)) {
+for (i in c(11:27, 30:37, 39:58, 60:67, 70:77, 80:86, 90:96)) {
   myURL <- paste0(base1, i, base2, id, base3)
   
   # get data set
@@ -38,7 +51,7 @@ for (i in c(11:27,30:58, 60:67, 70:77, 80:86, 90:96)) {
   contentDF <- fromJSON(textContent)
   
   output <- contentDF[,c(3,6,7,8)]
-  outputName <- paste0("/Data/monthlyCPI/monthlyCPI_", i, ".csv")
+  outputName <- paste0("/Data/monthlyCPI/monthlyCPI_",id,"_", i, ".csv")
   write.csv(output, outputName)
   
   msg2 <- paste0("Output data for province ", i)
@@ -47,30 +60,62 @@ for (i in c(11:27,30:58, 60:67, 70:77, 80:86, 90:96)) {
   rm(i, myURL, rawResult, msg1, textContent, contentDF, output, outputName, msg2)
 }
 
+base3 <- "000000000000&from_year=2012&to_year=2017"
 
-# print(myURL)
-# 
-# name
-# 
-# 
-# ?paste0
-# myURL <- "https://dataapi.moc.go.th/cpip-indexes?province_code=13&index_id=1220000000000000&from_year=2003&to_year=2017"
-# 
-# rawResult <- GET(myURL)
-# 
-# View(rawResult)
-# names(rawResult)
-# 
-# textContent <- rawToChar(rawResult$content)
-# contentDF <- fromJSON(textContent)
-# 
-# output <- contentDF[,c(3,6,7,8)]
-# 
-# write.csv(output, "/Data/monthlyCPI/monthlyCPI_13.csv")
-# 
-# head(contentDF)
-# 
-# head(textContent)
-# rawResult$content
-# ?GET
+for (i in 38:38) {
+  myURL <- paste0(base1, i, base2, id, base3)
+  
+  # get data set
+  rawResult <- GET(myURL)
+  msg1 <- paste0("Downloaded data for province ", i)
+  print(msg1)
+  
+  textContent <- rawToChar(rawResult$content)
+  contentDF <- fromJSON(textContent)
+  
+  output <- contentDF[,c(3,6,7,8)]
+  outputName <- paste0("/Data/monthlyCPI/monthlyCPI_",id,"_", i, ".csv")
+  write.csv(output, outputName)
+  
+  msg2 <- paste0("Output data for province ", i)
+  print(msg2)
+  
+  rm(i, myURL, rawResult, msg1, textContent, contentDF, output, outputName, msg2)
+}
 
+# Bangkok & vicinity 
+# (waiting for moc's reply to get Bangkok's data individually)
+
+base1 <- "https://dataapi.moc.go.th/cpig-indexes?region_id="
+base3 <- "000000000000&from_year=2003&to_year=2017"
+
+for (i in 0:0) {
+  myURL <- paste0(base1, i, base2, id, base3)
+  
+  # get data set
+  rawResult <- GET(myURL)
+  msg1 <- paste0("Downloaded data for province 10")
+  print(msg1)
+  
+  textContent <- rawToChar(rawResult$content)
+  contentDF <- fromJSON(textContent)
+  
+  output <- contentDF[,c(3,6,7,8)]
+  outputName <- paste0("/Data/monthlyCPI/monthlyCPI_",id,"_10", ".csv")
+  write.csv(output, outputName)
+  
+  msg2 <- paste0("Output data for province 10")
+  print(msg2)
+  
+  rm(i, myURL, rawResult, msg1, textContent, contentDF, output, outputName, msg2)
+}
+
+# combine all provinces ================================================================
+myPattern <- paste0("monthlyCPI_", id)
+  
+allFiles <- list.files("/Data/monthlyCPI", pattern = myPattern, full.names = TRUE)
+allFiles_list <- lapply(allFiles, read.csv, row.names=1)
+allFiles_output<- bind_rows(allFiles_list)
+
+outputName <- paste0("/Data/monthlyCPI/monthlyCPI_", id, ".dta")
+write_dta(allFiles_output, path = outputName)
